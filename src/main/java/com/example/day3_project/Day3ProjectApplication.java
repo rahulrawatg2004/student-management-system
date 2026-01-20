aaad04e05eea91ba447a31c0e726e867479991c5
Rahul rawat <rahul.rawat_cs23@gla.ac.in>
2026-01-20 11:50:58 +0530

    starting of project

---
+++ src/main/java/com/example/day3_project/Day3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3_project/Day3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
